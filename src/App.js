b50421542dc78263a03a5dce51bb3b382232daaa
zemikyafu <zemikfcb@gmail.com>
2024-07-16 16:09:25 +0300

    react frontend components; todosForm and todolist added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/Todos.css";
+import TodosForm from "./components/TodosForm";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
 
 function App() {
+  const [todoLists, setTodoLists] = useState([]);
+  const [formActive, setFormActive] = useState(false);
+  const [listActive, setListActive] = useState(false);
+
+  function openForm() {
+    if (!formActive) {
+      setFormActive(true);
+      setListActive(false);
+    } else {
+      setFormActive(false);
+      setListActive(true);
+    }
+  }
+
+  function onClickAdd(active, data) {
+    setTodoLists([...todoLists, data]);
+    setListActive(active);
+    setFormActive(!active);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="addButton" onClick={openForm}>
+        {" "}
+        add new todo
+      </button>
+      {formActive && <TodosForm onClickAdd={onClickAdd}></TodosForm>}
+      {listActive && <TodoList todolists={todoLists}></TodoList>}
     </div>
   );
 }
