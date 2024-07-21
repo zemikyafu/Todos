b6ce1791a7bbaf43282995d687d9d38a3e8df9b5
zemikyafu <zemikfcb@gmail.com>
2024-07-21 21:26:04 +0300

    rest api implementation added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,89 @@
 const express = require("express");
 const app = express();
+const todosServices=require('./services/todosService.js');
 
-app.get("/",(req,res)=>{
-res.set({'content-type':'text/html'})
-res.status(200).send('<p>Home page</p>')
+
+
+app.get('/todos', async (req, res) => {
+
+  todosServices.readtodos(file,(error,data)=>{
+    if(error)
+    {
+      console.log('error: ',error)
+      res.status(500).send({'Error':'Internal Server error'})
+    }
+    else
+    {
+      console.log("todos: ",JSON.parse(data));
+      res.status(200).json(JSON.parse(data).todos);
+    }
+    
+   });
+      
 });
-app.get("/toodos", (req, res) => {
-  const url = req.url;
-  console.log(url);
 
-  const todos = JSON.stringify({ id: 1, title: "test", status: "done" });
-  res.set({'content-type':'text/html'})
-  res.status(200).send(todos);
-  res.send(todos);
+app.get('/todos/:id',async (req,res)=>{
+    const id= parseInt(req.params.id);
+   await todosServices.readtodo(id,(error,data)=>{
+    if(error)
+    {
+      res.status(500).send({'error':'Internal server error'})
+      console.log('error: ',error)
+    }
+    else if(data)
+    {
+        res.status(200).json(data);
+     
+    }
+    else
+    {
+       res.status(404).json({"error":"Todo not found"})
+    }
+    
+   });
+  
 });
 
-app.listen("8080", () => console.log("Listing on port 8080"));
+app.put('/todo/:id',(req,res)=>{
+  const id = parseInt(req.params.id);
+  const newTodo=req.body;
+  todosServices.updateTodos(id,newTodo,(error,data)=>{
+    if(error)
+      {
+        res.status(500).send({'error':'Internal server error'})
+        console.log('error: ',error)
+      }
+      else if(data)
+      {
+          res.status(200).json(data);
+       
+      }
+      else
+      {
+         res.status(404).json({"error":"Todo not found"})
+      }
+      
+  })
+
+});
+
+app.post('/todos',(req,res)=>{
+  try{
+      const newTodos = todosServices.readtodos();
+      newTodos.push(data);
+      // await todosServices.writeFile(todos);
+      todosServices.writeFile(todos);
+      res.status(201).Json(newTodos);
+  }
+  catch(error)
+  {
+    console.log('Error right: ',error);
+    return [];
+
+  }
+  
+
+});
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listing on port ${port}`));
